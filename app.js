dc3ae1ef31bb626e2c085c0f00d6f2b38e868c93
Ogaga <iyaraogaga69@gmail.com>
2023-03-04 10:46:49 +0100

    Modified User Model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const helmet = require("helmet");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 // const tourRouter = require("./routes/tourRoutes");
-// const userRouter = require("./routes/userRoutes");
+const userRouter = require("./routes/userRoutes");
 // const viewRouter = require("./routes/viewRoutes");
 // const reviewRouter = require("./routes/reviewRoutes");
 // const bookingRouter = require("./routes/bookingRoutes");
@@ -108,7 +108,7 @@ app.use((req, res, next) => {
 // Routes
 // app.use("/", viewRouter);
 // app.use("/api/v1/tours", tourRouter);
-// app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 // app.use("/api/v1/reviews", reviewRouter);
 // app.use("/api/v1/bookings", bookingRouter);
 
