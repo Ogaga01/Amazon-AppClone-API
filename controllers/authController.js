cf519df1706f49cdb7512c6a2f73a0d5d109f5cf
Ogaga <iyaraogaga69@gmail.com>
2023-03-06 14:53:48 +0100

    Add Email pugs

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,7 @@ exports.signUp = catchAsyncError(async (req, res, next) => {
 
   const url = `${req.protocol}://${req.get("host")}/me`;
   // console.log(url);
-  // await new Email(newUser, url).sendWelcome();
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
