3918fc519897978ddd51b002dfc3c11bb0573b30
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-04-13 15:01:19 +0100

    Add products image folder

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,6 @@ const Product = require("./../models/productModel");
 const AppError = require("./../utils/appError");
 const catchAsyncError = require("./../utils/catchAsyncError");
 const factory = require("./handlerFactory");
-
 const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
@@ -14,34 +13,25 @@ const multerFilter = (req, file, cb) => {
     cb(new AppError("Not an image. Please upload a valid image", 400), false);
   }
 };
-
 const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
 });
-
 exports.uploadProductPhoto = upload.single("photo");
-
 exports.resizeProductPhoto = catchAsyncError(async (req, res, next) => {
-  if (!req.file.photo) return next();
+  if (!req.file) return next();
 
-  req.body.photo = `product-${req.params.id}-${Date.now()}.jpeg`;
+  req.file.filename = `product-${req.product.id}-${Date.now()}.jpeg`;
 
-  await sharp(req.file.photo[0].buffer)
+  await sharp(req.file.buffer)
     .resize(1000, 1000)
     .toFormat("jpeg")
     .jpeg({ quality: 100 })
-    .toFile(`public/img/products/${req.body.photo}`);
-
+    .toFile(`public/img/products/${req.file.filename}`);
   next();
 });
-
 exports.getAllProducts = factory.getAll(Product);
-
 exports.getProduct = factory.getOne(Product /*{ path: "reviews" }*/);
-
 exports.updateProduct = factory.updateOne(Product);
-
 exports.deleteProduct = factory.deleteOne(Product);
-
 exports.createProduct = factory.createOne(Product);
