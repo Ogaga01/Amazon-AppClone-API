1b891795eab4e700df6c0edbc8332ce3cb92b221
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-04-18 11:23:56 +0100

    Add default cart

--- models/userModel.js
+++ models/userModel.js
@@ -35,13 +35,13 @@ const userSchema = new mongoose.Schema({
       ],
       totalPrice: Number,
       totalQuantity: Number,
-      default: { products: [], totalPrice:{
+      default: [{ products: [], totalPrice:{
         type: Number,
         default: 0,
       }, totalQuantity: {
         type: Number,
         default: 0,
-      } },
+      } }],
     },
   ],
   role: {
